f7be43943678acf17c92980c397917082022438e
ravenusmc <mcuddy77@gmail.com>
2025-12-02 16:51:05 -0500

    working on landing page - title component

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    
+    <TitleArea></TitleArea>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import TitleArea from '@/components/home/TitleArea.vue'
 
 export default {
   name: 'HomeView',
+  components: {
+    TitleArea
+  }
 }
 </script>
