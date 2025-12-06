ee90dc8d3ac00f659413c7dcc6f19bc42dbd0e61
ravenusmc <mcuddy77@gmail.com>
2025-12-06 11:35:07 -0500

    added in navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,24 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <div>
+    <NavBar />
     <router-view/>
+    <!-- <FootComponent /> -->
   </div>
 </template>
 
+<script>
+import NavBar from "./components/common/NavBar.vue";
+// import FootComponent from "@/components/common/FootComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+    // FootComponent,
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
