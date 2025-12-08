15741a48dc9c0974ef40041cbfe4b0c386a9a86c
ravenusmc <mcuddy77@gmail.com>
2025-12-08 16:24:50 -0500

    working on landing page

--- client/src/components/home/TitleArea.vue
+++ client/src/components/home/TitleArea.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
-    TEST
+    <div class="jumbotron">
+      <div class="title-box">
+        <h1>Social Media and Mental Health</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: "TitleArea",
+}
+</script>
+
+<style scoped>
+
+.jumbotron {
+  width: 100%;
+  height: 650px;
+  border-radius: 10px;
+  background: url("../../assets/images/girl.jpg") center/cover no-repeat;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.7);
+  display: flex; 
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
