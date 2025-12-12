f22041115086be87510d7142ead500dceff74235
ravenusmc <mcuddy77@gmail.com>
2025-12-12 16:10:19 -0500

    minor change

--- client/src/components/home/Purpose.vue
+++ client/src/components/home/Purpose.vue
@@ -3,6 +3,12 @@
     <div class="main-div">
       <div>
         <h2>Purpose</h2>
+        <p>
+          This is another one of my projects that I choose by seeing an interesting data set on Kaggle. The project 
+          is on social media health. There seems to be an article everywhere these days on social media health and we 
+          all know it ain't good. The Anxious Generation by Jonathan Haidt really nails this. I deciided to do the project 
+          simply as a good way to look at a decent, small dataset. We will see where it goes! 
+        </p>
       </div>
       <div>
 
