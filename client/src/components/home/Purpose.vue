e20d2cb30954019038703d35ea224b4d61f3f218
ravenusmc <mcuddy77@gmail.com>
2025-12-13 12:12:10 -0500

    working on purpose area

--- client/src/components/home/Purpose.vue
+++ client/src/components/home/Purpose.vue
@@ -6,7 +6,7 @@
         <p>
           This is another one of my projects that I choose by seeing an interesting data set on Kaggle. The project 
           is on social media health. There seems to be an article everywhere these days on social media health and we 
-          all know it ain't good. The Anxious Generation by Jonathan Haidt really nails this. I deciided to do the project 
+          all know it ain't good. The Anxious Generation, by Jonathan Haidt, really nails this. I deciided to do the project 
           simply as a good way to look at a decent, small dataset. We will see where it goes! 
         </p>
       </div>
@@ -26,6 +26,8 @@
 
 .main-div {
   border: 2px solid red; 
+  display: grid; 
+  grid-template-columns: 1fr 1fr;
 }
 
 </style>
