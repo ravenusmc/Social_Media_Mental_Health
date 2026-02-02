3af903f5fb95db41294704ceac320e251112edda
ravenusmc <mcuddy77@gmail.com>
2026-02-02 16:53:32 -0500

    working on landing page

--- client/src/components/home/Purpose.vue
+++ client/src/components/home/Purpose.vue
@@ -3,14 +3,14 @@
     <div class="main-div">
       <div>
         <h2>Purpose</h2>
-        <p>
+        <p class="into-paragraph">
           This is another one of my projects that I choose by seeing an interesting data set on Kaggle. The project 
           is on social media health. There seems to be an article everywhere these days on social media health and we 
           all know it ain't good. The Anxious Generation, by Jonathan Haidt, really nails this. I deciided to do the project 
           simply as a good way to look at a decent, small dataset. We will see where it goes! 
         </p>
       </div>
-      <div>
+      <div class="picture-div">
 
       </div>
     </div>
@@ -28,6 +28,14 @@
   border: 2px solid red; 
   display: grid; 
   grid-template-columns: 1fr 1fr;
+}
+
+.into-paragraph {
+  font-size: 25px;
+}
+
+.picture-div {
+  
 }
 
 </style>
