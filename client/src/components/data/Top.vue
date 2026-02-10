09680581712368a8d22716862337d1cd8a84208d
ravenusmc <mcuddy77@gmail.com>
2026-02-10 16:47:48 -0500

    worked on title area for data page

--- client/src/components/data/Top.vue
+++ client/src/components/data/Top.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <h3>Graphs of the Data</h3>
+    <div class="title-div">
+      <h3>The Graphs</h3>
+    </div>
   </div>
 </template>
+
+<style scoped>
+
+.title-div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+h3 {
+  text-transform: uppercase;
+  font-size: 50px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.7);
+  padding: 1%;
+  border-radius: 15px;
+}
+
+</style>
