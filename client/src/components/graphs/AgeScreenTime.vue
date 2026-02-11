322fb1f7e31323da016f8d416a9a089f9785458a
ravenusmc <mcuddy77@gmail.com>
2026-02-11 16:40:09 -0500

    started work on first graph

---
+++ client/src/components/graphs/AgeScreenTime.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    test
+    <div ref="CauseOfDeathGraph"></div>
+
+    <!-- Popup -->
+    <!-- <div id="popup">
+      <div id="popupContent" class="popup-scroll"></div>
+      <button @click="closePopup">Close</button>
+    </div> -->
+  </div>
+</template>
+
+<script> 
+</script>
+
+<style scoped></style>
--- client/src/views/DataView.vue
+++ client/src/views/DataView.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <Top></Top>
+    <AgeScreenTime/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Top from '@/components/data/Top.vue'
+import AgeScreenTime from '@/components/graphs/AgeScreenTime.vue'
 
 export default {
   name: 'DataView',
   components: {
     Top, 
+    AgeScreenTime, 
   }
 }
 
