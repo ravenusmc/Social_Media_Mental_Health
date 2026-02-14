449f39d0a2394fb4b94376b2fa28cae29d98da3b
ravenusmc <mcuddy77@gmail.com>
2026-02-14 14:32:11 -0500

    set up flask file and file to examine data set

---
+++ server/data.py
@@ -0,0 +1,10 @@
+#Main file to examine the CSV data. 
+
+# importing supporting libraries
+import numpy as np
+import pandas as pd
+import calendar
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+class ExamineData():
--- server/main.py
+++ server/main.py
@@ -1,3 +1,18 @@
-https://www.kaggle.com/datasets/ayeshaimran123/social-media-and-mental-health-balance
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import math
+
+# Importing files that I made:
+# from data import *
+# from db import *
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
