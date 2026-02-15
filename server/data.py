8c3b43461a63e7afa4573ded4d31d9f4788cced0
ravenusmc <mcuddy77@gmail.com>
2026-02-15 13:08:03 -0500

    working on age vs screen time graph

--- server/data.py
+++ server/data.py
@@ -8,3 +8,30 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 class ExamineData():
+  
+  def __init__(self): 
+    self.data = pd.read_csv('./data/Mental_Health_Dataset.csv')
+
+  def test(self):
+    print(self.data.head())
+  
+  def AgeVsScreenTime(self):
+    pass
+
+
+
+
+## 📈 7. Age vs. Screen Time (Scatter Plot)
+
+# **Type:** Scatter Plot
+# **X-axis:** Age
+# **Y-axis:** Daily_Screen_Time
+
+# **Purpose:**
+# Shows generational differences in screen habits.
+
+# **Possible Finding:**
+# Younger users → more screen time
+
+obj = ExamineData()
+obj.test()
