25c2aadb8901eac595e2188b15ae6dca3944b0ef
ravenusmc <mcuddy77@gmail.com>
2026-03-23 17:01:33 -0400

    working on fourth graph on client side

--- client/src/components/graphs/DetoxVsHappiness.vue
+++ client/src/components/graphs/DetoxVsHappiness.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-
+    <div ref="DetoxVsHappinessGraph"></div>
   </div>
 </template>
 
 <script>
+import * as d3 from "d3";
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "DetoxVsHappiness",
+  computed: {
+    ...mapGetters("datapage", ["detoxDaysVsHappiness"]),
+  },
+  mounted() {
+    this.buildDetoxVsHappinessGraph();
+  },
+  methods: {
+    buildDetoxVsHappinessGraph() {
+      
+    }
+  }
+}
 
 </script>
