0afdc6e9eb3fd34e213acd7ec4ba421ad38dea77
ravenusmc <mcuddy77@gmail.com>
2026-03-24 16:53:07 -0400

    working on graph for client sie

--- client/src/components/graphs/DetoxVsHappiness.vue
+++ client/src/components/graphs/DetoxVsHappiness.vue
@@ -18,6 +18,14 @@ export default {
   },
   methods: {
     buildDetoxVsHappinessGraph() {
+
+      // Remove old SVG
+      d3.select(this.$refs.DetoxVsHappinessGraph).selectAll("*").remove();
+
+      const margin = { top: 50, right: 30, bottom: 50, left: 70 };
+      const width = 460 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+
       
     }
   }
