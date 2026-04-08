0240706295492308d51413e631c056a07dc8a65a
ravenusmc <mcuddy77@gmail.com>
2026-04-08 16:54:59 -0400

    working on d3 for fifth graph

--- client/src/components/graphs/ExerciseVsHappiness.vue
+++ client/src/components/graphs/ExerciseVsHappiness.vue
@@ -13,8 +13,31 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "ExerciseVsSleep",
   computed: {
-    ...mapGetters("datapage", ["sleepvsstressData"]),
+    ...mapGetters("datapage", ["ExerciseVsHappiness"]),
   },
+  mounted() {
+    this.buildExerciseVsHappinessGraph();
+  },
+  methods: {
+    buildExerciseVsHappinessGraph() {
+
+      // Remove old SVG
+      d3.select(this.$refs.ExerciseVsHappinessGraph).selectAll("*").remove();
+
+      const margin = { top: 50, right: 30, bottom: 50, left: 70 };
+      const width = 460 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+
+      const svg = d3
+        .select(this.$refs.ExerciseVsHappinessGraph)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    }
+  }
 
 }
 
