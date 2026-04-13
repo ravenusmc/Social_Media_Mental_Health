eb489e75a7a98e0d70c44e67b540270877960be3
ravenusmc <mcuddy77@gmail.com>
2026-04-13 16:21:54 -0400

    added labels

--- client/src/components/graphs/ExerciseVsHappiness.vue
+++ client/src/components/graphs/ExerciseVsHappiness.vue
@@ -68,14 +68,14 @@ export default {
         .attr("cy", d => y(d[1]))
         .attr("r", 4);
       
-      // Graph title 
+      // X-Axis Label 
       svg
         .append("text")
         .attr("x", width / 2)
         .attr("y", height + margin.bottom - 10)
         .attr("text-anchor", "middle")
         .attr("font-weight", "bold")
-        .text("Exercise VS Happiness");
+        .text("Exercise (Hours)");
       
       // Y-Axis Label 
       svg
@@ -85,7 +85,16 @@ export default {
         .attr("y", -margin.left + 20)
         .attr("text-anchor", "middle")
         .attr("font-weight", "bold")
-        .text("Stress (1-10)");
+        .text("Happiness (1-10)");
+      
+      // Title 
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", -margin.top / 2 + 10)
+        .attr("text-anchor", "middle")
+        .attr("font-weight", "bold")
+        .text("Exercise VS Happeiness (Graph 5)");
 
     }
   }
