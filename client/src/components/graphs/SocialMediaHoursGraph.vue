d17c75bee75cebb76d8d3f8797a07f5b25839e40
ravenusmc <mcuddy77@gmail.com>
2026-04-28 16:12:26 -0400

    working on social media hours graph

--- client/src/components/graphs/SocialMediaHoursGraph.vue
+++ client/src/components/graphs/SocialMediaHoursGraph.vue
@@ -22,6 +22,18 @@ export default {
       
       // Remove old SVG
       d3.select(this.$refs.SocialMediaHoursGraph).selectAll("*").remove();
+
+      const margin = { top: 50, right: 30, bottom: 50, left: 70 };
+      const width = 460 - margin.left - margin.right;
+      const height = 400 - margin.top - margin.bottom;
+
+      const svg = d3
+        .select(this.$refs.socialMediaHours)
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
     },
   },
 }
